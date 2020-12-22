050443ad647b3c5576321c51f41de04255bc2d51
Giss12 <36027903+Giss12@users.noreply.github.com>
2020-12-22 12:07:19 +0300

    Add files via upload

---
+++ AbstractClass.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+abstract class AbstractClass
+{
+    protected abstract int AbstractA(string s);
+    protected abstract int AbstractB();
+    public virtual int VirtualA(int a, int b)
+    {
+        return a + b;
+    }
+    public virtual int VirtualB(int c)
+    {
+        return c ^ 2;
+    }
+
+    public int TemplateMethod(string s, int a, int b, int c)
+    {
+        return AbstractA(s) ^ AbstractB() + VirtualA(a, b) * VirtualB(c);
+    }
+}
+
+class OverridenA : AbstractClass
+{
+    protected override int AbstractA(string s)
+    {
+        return s.Length;
+    }
+
+    protected override int AbstractB()
+    {
+        return 42;
+    }
+}
+
+class OverridenB : AbstractClass
+{
+    protected override int AbstractA(string s)
+    {
+        string vowels = "ауоыиэяюёе";
+        int result = 0;
+        foreach (char symbol in s)
+        {
+            if (vowels.Contains(symbol.ToString()))
+                result++;
+        }
+        return result;
+    }
+
+    protected override int AbstractB()
+    {
+        int a = Random.Range(0, 1);
+        return a;
+    }
+    public override int VirtualA(int a, int b)
+    {
+        return a * b;
+    }
+    public override int VirtualB(int c)
+    {
+        return c % 7;
+    }
+}
---
+++ ObsA.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObsA : MonoBehaviour
+{
+    OverridenA overA = new OverridenA();
+    OverridenB overB = new OverridenB();
+    private void Start()
+    {
+        avA();
+    }
+
+    void avA()
+    {
+        string a;
+        a = overA.TemplateMethod("Ты", 1, 2, 3).ToString();
+        Debug.Log(a);
+    }
+    void avB()
+    {
+        string a;
+        a = overB.TemplateMethod("Вы", 4, 5, 6).ToString();
+        Debug.Log(a);
+    }
+}
